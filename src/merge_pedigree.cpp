dce91db891bd4e32567646f11202592bb75c1c28
Mikkel Meyer Andersen <mikl@mikl.dk>
2019-05-17 10:55:08 +0200

    work

--- src/merge_pedigree.cpp
+++ src/merge_pedigree.cpp
@@ -9,6 +9,126 @@
 #include "util.h"
 #include "util-validate.h"
 
+/*
+ * P: ped with PoI
+ * x: surrogate ancestor
+ * o: existing founders
+ * 
+ * ---------------------------------------------------------------------
+ * METHODS
+ * ---------------------------------------------------------------------
+ * METHOD 1 (rejection):
+ *   Parameters: `max_rejections`
+ *   
+ * METHOD 2 (relaxation)
+ *   Parameters: `max_years_diff`
+ *   
+ * ---------------------------------------------------------------------
+ * Initial step: We have only P.
+ * ---------------------------------------------------------------------
+ * 
+ * `sample_ancestor_with_children(P)`:
+ *   a) Sample a sons configuration (number of sons and ages).
+ *   b) Randomly pick which of the sons P is. 
+ *      Now birthyear of new surrogate ancestor can be calculated.
+ *      And birthyear of the other sons than be can be calculated.
+ *   c) Sample among the other existing pedigree founders some with those birthyears.
+ *        + If none exists:
+ *            METHOD 1 (rejection):
+ *              Go to a) 
+ *              [but only `max_rejections` number of times]
+ *            
+ *            METHOD 2 (relaxation):
+ *              Try birthyear at distance one, distance two, ... 
+ *              [but only up to `max_years_diff`]
+ *          
+ *   Now we have (e.g. for three sons):
+ *
+ *        x
+ *      / | \
+ *     P  o  o
+ * 
+ * ---------------------------------------------------------------------
+ * Induction step: Attach new surrogate ancestor to surrogate ancestor.
+ * ---------------------------------------------------------------------
+ * 
+ * Let Q be the founder.
+ * 
+ *        Q
+ *      / | \
+ *     P  o  o
+ * 
+ *  Same as base step above for Q = P:
+ *  Except that some new surrogate children must be added.
+ *  
+ * `sample_ancestor_with_children(Q)`:
+ *   a) Sample a sons configuration (number of sons and ages).
+ *   b) Randomly pick which of the sons Q is. 
+ *      Now birthyear of new surrogate ancestor can be calculated.
+ *      And birthyear of the other sons than be can be calculated.
+ *   c) Sample among the other existing pedigree founders some with those birthyears.
+ *        + If none exists:
+ *            METHOD 1 (rejection):
+ *              Go to a) 
+ *              [but only `max_rejections` number of times]
+ *            
+ *            METHOD 2 (relaxation):
+ *              Try birthyear at distance one, distance two, ... 
+ *              [but only up to `max_years_diff`]
+ *          
+ *   Now we have (e.g. for three sons):
+ *   
+ *            x _____
+ *          /  \      \
+ *        Q      x     x
+ *      / | \
+ *     P  o  o
+ *     
+ *  Let y != Q be children to the new founder (except Q):
+ *
+ *
+ * Generation
+ * 
+ * 2         x  _____
+ *          /  \      \
+ * 1       Q      y     y
+ *       / | \
+ * 0    P  o  o
+ *  
+ *  
+ *  For each y:
+ *  `sample_descendents(y)`:   
+ *   a) If `generation(y) > generation(P) + 1`
+ *        Sample a sons configuration (number of sons and ages).
+ *        Create surrogate individuals, z, as children to y.
+ *        Call `sample_descendents(z)`
+ *   b) If `generation(y) = generation(P) + 1`
+ *        Step c) from `sample_ancestor_with_children`:
+ *        I.e. attach to existing pedigrees.
+ *        
+ *   TODO:
+ *   HOW DOES DISCRETE GENERATIONS WORK WITH THIS?
+ *   DO WE NEED GENERATION AND BIRTHYEAR? OR JUST BIRTHYEAR?
+ *   FIXME: JUST BIRTHYEAR!
+ *          TRY TO SAMPLE INDIVIDUALS WITH NEEDED BIRTHYEARS. 
+ *          IF NONE EXISTS, INTRODUCE SURROGATE.
+ *          WITH SOME SURROGATE ANCESTORS, WE KNOW THAT SURROGATE MUST BE INTRODUCED.
+ *          BUT THERE CAN BE BIAS?!
+ *          PARAMETER CONTROLLING THIS?
+ *     
+ *     
+ *  Always two sons:
+ * 
+ *             x
+ *        ____/ \____
+ *       x           x
+ *      / \         / \
+ *    x     x     x     x
+ *   / \   / \   / \   / \
+ *  P   o o   o o   o o   o
+ */
+
+
 //' Merge a pedigree randomly
 //' 
 //' Note that children will be added according to `sons_configs`.
